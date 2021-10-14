3302f774a3141c32e4b28af9e3365b98937bcbee
Mickael Lanfranchi <58193062+cecceccu@users.noreply.github.com>
2021-10-14 18:19:45 +0200

    Added class to list concrete implementations of functoids

---
+++ Services/FunctoidEnumerator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public static class FunctoidEnumerator
+    {
+        static FunctoidEnumerator() { }
+
+        public static List<System.Type> GetFunctoidClasses()
+        {
+            var objects = (
+                    from domainAssembly in AppDomain.CurrentDomain.GetAssemblies()
+                    from assemblyType in domainAssembly.GetTypes()
+                    where
+                    typeof(ServiceContracts.Functoid).IsAssignableFrom(assemblyType)
+                    && !assemblyType.IsAbstract
+                    select assemblyType).ToList();
+            return objects;
+        }
+    }
+}
