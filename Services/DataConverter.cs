eb9625bef6c39f2966941167b814c06ec7563455
MohamedDib <49766520+MohamedDib@users.noreply.github.com>
2021-10-15 14:37:46 +0200

    DataConverter : apply a template on columns

---
+++ Services/DataConverter.cs
@@ -0,0 +1,31 @@
+﻿using DataContracts;
+using Services.ServiceContracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    class DataConverter
+    {
+        public List<Column> inData { get; set; }
+        public List<Column> outData { get; set; }
+        public Template template { get; set; }
+
+        public void Converter()
+        {
+            
+            foreach (Column col in inData)
+            {
+                foreach (Functoid f in template.functoids)
+                {
+                    outData.AddRange(f.Transform());
+                }
+            }
+            
+        }
+    }
+
+}
