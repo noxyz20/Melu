c84e9ede441a3c29d76d80dd4b48bd5e9fcd5546
Notsuko <teinturier.theo@gmail.com>
2021-10-15 09:43:47 +0200

    Fill combobox with functoids.

--- Configurator/Form1.cs
+++ Configurator/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Services;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -34,6 +35,13 @@ namespace Configurator
 
         private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
         {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.DataSource = FunctoidEnumerator.GetFunctoidClasses();
+            comboBox1.DisplayMember = "name";
 
         }
     }
