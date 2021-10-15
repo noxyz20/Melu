41794e3e7ffb6155f9301563ae28531aef459c0e
Mickael Lanfranchi <58193062+cecceccu@users.noreply.github.com>
2021-10-15 11:04:15 +0200

    Deleted Service Contracts project and refactored Constant funtoid

--- Services/Constant.cs
+++ Services/Constant.cs
@@ -1,5 +1,5 @@
 ﻿using DataContracts;
-using ServiceContracts;
+using Services.ServiceContracts;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,7 +10,7 @@ namespace Services
 {
     public class Constant:Functoid
     {
-        public override List<Column> Transform(List<Column> columnList, List<Parameter> parameters)
+        public override List<Column> Transform()
         {
             var val = parameters[0].value;
             columnList.Select(@col => @col.cells.Select(
