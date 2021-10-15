62526e11caf54ff1d78d42ab5bb77a33edaf0a2b
Storm <funnygames350@gmail.com>
2021-10-15 11:33:08 +0200

    xls reader returns columns list

--- Services/Reader.cs
+++ Services/Reader.cs
@@ -4,21 +4,46 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using IronXL;
+using DataContracts;
 
 namespace Services
 {
     public class Reader
-    {   
+    {
         public void readXls(String filePath)
         {
             var workbook = new WorkBook(filePath);
             var sheet = workbook.DefaultWorkSheet;
-            var i = 0;
-            foreach (var column in sheet.Columns)
+            var columns = new List<Column>();
+            var firstLine = true;
+            foreach (var row in sheet.Rows)
             {
-
-                var row = column.Value;
-                i++;
+                if (row.Columns[0].Value.ToString() == "") break;
+                var i = 0;
+                foreach (var column in row.Columns)
+                {
+                    var cell = column.Value.ToString();
+                    if (firstLine)
+                    {
+                        columns.Add(new Column()
+                        {
+                            name = cell,
+                            cells = new List<DataContracts.Cell>(),
+                            id = Guid.NewGuid()
+                        });
+                    }
+                    else
+                    {
+                        columns[i].cells.Add(new DataContracts.Cell()
+                        {
+                            content = cell
+                        });
+                       
+                    }
+                    i++;
+                    
+                }
+                firstLine = false;
             }
         }
 
